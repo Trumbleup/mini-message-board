b4b18de13ef28afd1935df08d66a9ba65d3141ca
Danny <Dtrumble12@gmail.com>
2020-08-31 18:57:03 -0500

    Added the form functionality and setup PUG templates

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [
+   {
+     text: "Hi there!",
+     user: "Amando",
+     added: new Date()
+   },
+   {
+     text: "Hello World!",
+     user: "Charles",
+     added: new Date()
+   }
+];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: "Mini Messageboard", messages: messages });
+});
+router.post('/new', function(req, res, next) {
+  const { user, text } = req.body;
+  const date = new Date();
+  messages.push({'user': user, 'text': text, 'added': date});
+  res.redirect('/');
 });
 
 module.exports = router;
